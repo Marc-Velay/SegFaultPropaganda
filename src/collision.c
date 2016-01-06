16cf9b288ddd1f20a42c09e6ed5e642bb78e9d37
sauceaubeurre <romain.seusse@gmail.com>
2016-01-06 11:48:30 +0100

    update collision

--- src/collision.c
+++ src/collision.c
@@ -4,10 +4,10 @@ void collision()
 {
     int i;
     for(i=0; i<=Game.nbEnnemiCreated;i++){
-		if((Officer[i].x <= Tir.x) && ((Officer[i].x + GRID_STEP) >= Tir.x) && (Officer[i].y <= Tir.y) && ((Officer[i].y + GRID_STEP) >= Tir.y))
+		if((Officer[i].x <= Tir.x) && ((Officer[i].x + (GRID_STEP/2)) >= Tir.x) && (Officer[i].y <= Tir.y) && ((Officer[i].y + GRID_STEP) >= Tir.y))
             {
 			Officer[i].alive = 0;
-            Tir.on = 0;
+            Tir.on = Tir.x = Tir.y = Officer[i].x = Officer[i].y = 0;
 		}
 	}
 
