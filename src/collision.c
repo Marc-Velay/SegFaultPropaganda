6f6fda314d81fdd26feb1b71afded68d3b2e344f
Marc-Velay <marc.velay@gmail.com>
2016-01-09 12:36:50 +0100

    broken tir

--- src/collision.c
+++ src/collision.c
@@ -2,13 +2,20 @@
 
 void collision()
 {
-    int i;
-    for(i=0; i<=Game.nbEnnemiCreated;i++){
-		if( ( ( (Officer[i].x <= Tir.x) && ( (Officer[i].x + (GRID_STEP/5)) >= Tir.x) ) || ( (Officer[i].x <= Tir.x + 3*GRID_STEP/4) && ( (Officer[i].x + (GRID_STEP/5) ) >= Tir.x + GRID_STEP/2) ) ) && ((Officer[i].y - GRID_STEP/2) <= Tir.y) && ((Officer[i].y + GRID_STEP) >= Tir.y))
-	          {
-			Officer[i].alive = 0;
-			Tir.on = Tir.x = Tir.y = Officer[i].x = Officer[i].y = 0;
-		}
+	int i;
+	tir *newTir =Tir;
+	for(i=0; i<=Game.nbEnnemiCreated;i++){
+		 while(newTir != NULL){
+			 if( ( ( (Officer[i].x <= (*newTir).x) && ( (Officer[i].x + (GRID_STEP/5)) >= (*newTir).x) ) || ( (Officer[i].x <= (*newTir).x + 3*GRID_STEP/4) && ( (Officer[i].x + (GRID_STEP/5) ) >= (*newTir).x + GRID_STEP/2) ) ) && ((Officer[i].y - GRID_STEP/2) <= (*newTir).y) && ((Officer[i].y + GRID_STEP) >= (*newTir).y)) {
+				Officer[i].hpOfficer -=1;
+				if(Officer[i].hpOfficer <=0){
+					Officer[i].alive = 0;
+					Officer[i].x = Officer[i].y = 0;
+				}			
+				(*newTir).on = (*newTir).x = (*newTir).y = 0;
+			}
+		 }
+		
 	}
 
 }
