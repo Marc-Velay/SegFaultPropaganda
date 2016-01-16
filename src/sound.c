eef2269f117832736932e38b4a4d776a18e5c1ef
Marc-Velay <marc.velay@gmail.com>
2016-01-16 15:58:57 +0100

    deleted

--- src/sound.c
+++
@@ -1,9 +0,0 @@
-#include "fonctions.h"
-
-void loadAllMusic(){
-
-    musiqueLaser = Mix_LoadMUS("sde/Laser.aiff");
-    musiqueMenu = Mix_LoadMUS("sde/Menu.mp3");
-    Mix_VolumeMusic(MIX_MAX_VOLUME/5);
-}
-
